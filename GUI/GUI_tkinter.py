fcc09fcc9c5ac9a68f4a2af0a81e58e07b403d34
Anmol-111 <anmolprakash9213@gmail.com>
2025-02-05 23:54:17 +0530

    Add play() window

--- GUI/GUI_tkinter.py
+++ GUI/GUI_tkinter.py
@@ -2,14 +2,50 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 def play():
-    pass
-
+    window = tk.Toplevel()
+    window.title("Number Guessing Game")
+    image = Image.open("C:\\Users\\Anmol\\Desktop\\istockphoto-1227598160-612x612.jpg")
+    image = image.resize((800, 500), )
+    bg_image = ImageTk.PhotoImage(image)
+    canvas = tk.Canvas(window, width=image.width, height=image.height)
+    canvas.pack(fill="both", expand=True)
+    canvas.create_image(0, 0, image=bg_image, anchor="nw")
+    canvas.image = bg_image
+    lbl_game_name = tk.Label(window, text="Number Guessing Game", font=("Roboto", 22, "bold"), fg="white",
+                             bg="midnight blue", bd=5)
+    lbl_game_name.place(x=240, y=30)
+    window.mainloop()
+'''
 def game_history():
-    pass
+    window = tk.Toplevel()
+    window.title("Number Guessing Game")
+    image = Image.open("C:\\Users\\Anmol\\Desktop\\istockphoto-1227598160-612x612.jpg")
+    image = image.resize((800, 500), )
+    bg_image = ImageTk.PhotoImage(image)
+    canvas = tk.Canvas(window, width=image.width, height=image.height)
+    canvas.pack(fill="both", expand=True)
+    canvas.create_image(0, 0, image=bg_image, anchor="nw")
+    canvas.image = bg_image
+    lbl_game_name = tk.Label(window, text="Number Guessing Game", font=("Roboto", 22, "bold"), fg="white",
+                             bg="midnight blue", bd=5)
+    lbl_game_name.place(x=240, y=30)
+    window.mainloop()
 
 def game_exit():
-    pass
-
+    window = tk.Toplevel()
+    window.title("Number Guessing Game")
+    image = Image.open("C:\\Users\\Anmol\\Desktop\\istockphoto-1227598160-612x612.jpg")
+    image = image.resize((800, 500), )
+    bg_image = ImageTk.PhotoImage(image)
+    canvas = tk.Canvas(window, width=image.width, height=image.height)
+    canvas.pack(fill="both", expand=True)
+    canvas.create_image(0, 0, image=bg_image, anchor="nw")
+    canvas.image = bg_image
+    lbl_game_name = tk.Label(window, text="Number Guessing Game", font=("Roboto", 22, "bold"), fg="white",
+                             bg="midnight blue", bd=5)
+    lbl_game_name.place(x=240, y=30)
+    window.mainloop()
+'''
 def main_page():
     global window
     window = tk.Tk()
