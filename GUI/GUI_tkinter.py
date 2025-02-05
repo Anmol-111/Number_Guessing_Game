5663b9d48df45c1d08f09ce0980681990706838c
Anmol-111 <anmolprakash9213@gmail.com>
2025-02-05 23:02:30 +0530

    Add main-page using tkinter

---
+++ GUI/GUI_tkinter.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+def main_page():
+    global window
+    window = tk.Tk()
+    window.title("Number Guessing Game")
+    window.mainloop()
+
+if __name__ == "__main__":
+    main_page()
