768b9c4fbf59a78b634e379840744bdab6a53da2
Anmol-111 <anmolprakash9213@gmail.com>
2025-02-05 23:17:16 +0530

    Add bg image of the canvas in tkinter window

--- GUI/GUI_tkinter.py
+++ GUI/GUI_tkinter.py
@@ -1,9 +1,18 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+
 
 def main_page():
     global window
     window = tk.Tk()
     window.title("Number Guessing Game")
+    image = Image.open("C:\\Users\\Anmol\\Desktop\\istockphoto-1227598160-612x612.jpg")
+    image = image.resize((800, 500), )
+    bg_image = ImageTk.PhotoImage(image)
+    canvas = tk.Canvas(window, width=image.width, height=image.height)
+    canvas.pack(fill="both", expand=True)
+    canvas.create_image(0, 0, image=bg_image, anchor="nw")
+    canvas.image = bg_image
     window.mainloop()
 
 if __name__ == "__main__":
